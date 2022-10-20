cec607dc7dbb4c1f70f29dde5603e63ee75dfff3
nosianuri <nurinosia@gmail.com>
2022-10-20 15:30:36 +0600

    added brand

--- src/Components/Section2/Brand.js
+++ src/Components/Section2/Brand.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import './Brand.css';
+import netflix from "../../images/brands/netflix.svg";
+import nestle from "../../images/brands/Nestlé.svg";
+import amexpress from "../../images/brands/am-express.svg";
+import canon from "../../images/brands/canon.svg";
+import figma from "../../images/brands/figma.svg";
+import starbucks from "../../images/brands/starbucks.svg";
 import dot from "../../images/brands-dot.svg";
 
 const Brand = () => {
-    // const brandLogoList = [figma,, netflix, nestle, amexpress, canon, starbucks];
-  return (
-    <div className='brands-container'>
-        <div className='brand-heading'>
-            <h2>I am proud to have these amazing brands as my Clients</h2>
-            <img src={dot} alt="" />
+    const brandLogoList = [figma, , netflix, nestle, amexpress, canon, starbucks];
+    return (
+        <div className='brands-container'>
+            <div className='brand-heading'>
+                <h2>I am proud to have these amazing brands as my clients</h2>
+                <img src={dot} alt="" />
+            </div>
+            <div className='brand-logo-list'>
+                {brandLogoList.map((logo) => (<div className='brand-logo-container'>
+                    <img className='brand-logo' src={logo} alt="" />
+                </div>))}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
-export default Brand
+export default Brand;
